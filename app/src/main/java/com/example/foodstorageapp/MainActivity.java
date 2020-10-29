e4da73c77f24dd82d03184811096fd4023aaf5e3
Chad L <72481913+clookcode@users.noreply.github.com>
2020-10-28 22:06:40 -0400

    cleaned up comments from teach 07 activity

--- app/src/main/java/com/example/foodstorageapp/MainActivity.java
+++ app/src/main/java/com/example/foodstorageapp/MainActivity.java
@@ -11,16 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //Chad comment
-    //This is a comment from Nathan Kempton.
-    //This is John's comment
-    /*This is Ricardo's comment. haha*/
-    //merge for whatever
-    //For kicks and giggles.
-    //Testing merge conflict resolution
-    //Nathan can't wait his turn
-    //this is a comment
-    //one change
-    /*This is Ricardo's comment.*/
-    //merge for whatever
 }
