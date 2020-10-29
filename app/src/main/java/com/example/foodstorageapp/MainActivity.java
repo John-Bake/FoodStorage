7fe5bf26aa363dbd414ab55c9ec0930c63bb4ef3
Ricky <72452852+rnunez22@users.noreply.github.com>
2020-10-28 19:11:38 -0600

    Ricardo's comment....

--- app/src/main/java/com/example/foodstorageapp/MainActivity.java
+++ app/src/main/java/com/example/foodstorageapp/MainActivity.java
@@ -15,5 +15,5 @@ public class MainActivity extends AppCompatActivity {
     //This is a comment from Nathan Kempton.
     //This is John's comment
     /*This is Ricardo's comment.*/
-    //merge conflict!!!
+    //merge for whatever
 }
