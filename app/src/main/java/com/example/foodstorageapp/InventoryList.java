32ee9236fa8f06a03175cc1d1a070617fb956af3
Chad L <72481913+clookcode@users.noreply.github.com>
2020-11-04 21:19:02 -0500

    Added stub InventoryList class Added StorageItemTest class

---
+++ app/src/main/java/com/example/foodstorageapp/InventoryList.java
@@ -0,0 +1,22 @@
+package com.example.foodstorageapp;
+
+import java.util.List;
+
+public class InventoryList<storageItem> {
+
+    private List<StorageItem> inventory;
+
+    StorageItem storageItem = new StorageItem();
+
+    public StorageItem getItem(storageItem) {
+        return this.storageItem;
+    }
+
+    public void addItem(List<StorageItem>) {
+
+    }
+
+    public void removeItem(List<StorageItem>) {
+
+    }
+}
---
+++ app/src/test/java/com/example/foodstorageapp/StorageItemTest.java
@@ -0,0 +1,13 @@
+package com.example.foodstorageapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StorageItemTest {
+
+    @Test
+    public void setUnitOfMeasure() {
+
+    }
+}
