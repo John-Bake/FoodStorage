3c737bd9d2d7c6f2218a27b67a72cb85a825d30a
Chad L <72481913+clookcode@users.noreply.github.com>
2020-11-04 21:46:06 -0500

    Modified stub InventoryList class

--- app/src/main/java/com/example/foodstorageapp/InventoryList.java
+++ app/src/main/java/com/example/foodstorageapp/InventoryList.java
@@ -1,22 +1,21 @@
 package com.example.foodstorageapp;
 
-import java.util.List;
+import java.util.ArrayList;
 
-public class InventoryList<storageItem> {
+public class InventoryList {
 
-    private List<StorageItem> inventory;
+        //using ArrayList type as multiple threads can access simultaneously
+    private ArrayList<StorageItem> inventory;
 
-    StorageItem storageItem = new StorageItem();
-
-    public StorageItem getItem(storageItem) {
-        return this.storageItem;
+    public StorageItem getItem(ArrayList<StorageItem> inventory) {
+        return null;
     }
 
+    /*
     public void addItem(List<StorageItem>) {
-
     }
-
     public void removeItem(List<StorageItem>) {
-
     }
+    */
+
 }
