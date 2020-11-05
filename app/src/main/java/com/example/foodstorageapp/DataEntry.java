723d9942ab87af5cfe2efa76632c1c97de617d8d
Nathan <nathan.sdhsi@gmail.com>
2020-11-04 18:57:38 -0700

    DataEntry class created

---
+++ app/src/main/java/com/example/foodstorageapp/DataEntry.java
@@ -0,0 +1,32 @@
+package com.example.foodstorageapp;
+
+import java.util.List;
+
+public class DataEntry {
+    StorageItem newItem;
+    String fileName;
+    List<Object> importedArray;
+
+    public void drawImputForm() {}
+
+    public void reportSuccess() {}
+
+    public void reportError() {}
+
+    public String validateData(StorageItem itemToValidate) {
+        return "empty method";
+    }
+
+    public StorageItem scan() {
+        newItem = new StorageItem();
+        return newItem;
+    }
+
+//    public List<StorageItem> importArray() {
+ //       StorageItem s = new StorageItem();
+  //      importedArray.add(s);
+   //     return importedArray;
+    //}
+
+    public void saveToDatabase(StorageItem newItem) {}
+}
