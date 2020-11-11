37d78e606d4d2b72889bd338ca71957407033884
Ricky <72452852+rnunez22@users.noreply.github.com>
2020-11-10 21:22:44 -0700

    ricardo's classes RN-Gradle update for library. RN-Some work on FoodCategoryPresenter

--- app/src/main/java/com/example/foodstorageapp/FoodCategoryPresenter.java
+++ app/src/main/java/com/example/foodstorageapp/FoodCategoryPresenter.java
@@ -1,9 +1,19 @@
 package com.example.foodstorageapp;
 
+import java.util.LinkedList;
+
 public class FoodCategoryPresenter {
 
-    void displayList(){
-
+    public static void displayList(String[] args){
+        LinkedList<String> typeOfFood = new LinkedList<String>();
+        typeOfFood.add("Fruits");
+        typeOfFood.add("Veggies");
+        typeOfFood.add("Grains");
+        typeOfFood.add("Protein (meat and beans)");
+        typeOfFood.add("Spices");
+        typeOfFood.add("Water");
+        typeOfFood.add("Cleaning Supplies/Toiletries");
+        System.out.println(typeOfFood);
     }
 
 }
