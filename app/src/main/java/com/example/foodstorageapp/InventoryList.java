e87bc344ad7fe6b963be60509edaa2416fa07464
Chad L <72481913+clookcode@users.noreply.github.com>
2020-11-26 22:11:17 -0500

    Some Javadocs added to InventoryList class

--- app/src/main/java/com/example/foodstorageapp/InventoryList.java
+++ app/src/main/java/com/example/foodstorageapp/InventoryList.java
@@ -3,14 +3,25 @@ package com.example.foodstorageapp;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ */
 public class InventoryList extends StorageItem {
 
+    /**
+     * The ArrayList to store all the inventory items in
+     */
     private List<StorageItem> inventory = new ArrayList<StorageItem>();
 
     /*public InventoryList(ArrayList<StorageItem> testInventory) {
         inventory = testInventory;
     }*/
 
+    /**
+     * This function gets an item from the inventory list
+     * @param index where in the inventory array the item is
+     * @return the storage item from the inventory array.
+     */
     public StorageItem getItem (int index) {
                 //more code to go here
         //using ArrayList type as multiple threads can access simultaneously
@@ -18,11 +29,20 @@ public class InventoryList extends StorageItem {
         return inventory.get(index);
     }
 
+    /**
+     * This function adds an item to the inventory list
+     * @param newItem the item to be added to the inventory list
+     */
+
     public void addItem(StorageItem newItem) {
         //add an item to the inventory
         inventory.add(0,newItem);
     }
 
+    /**
+     * This function removes and item from the inventory list
+     * @param storageItem the item to be removed
+     */
     public void removeItem(StorageItem storageItem) {
         //remove an item from the inventory
     }
